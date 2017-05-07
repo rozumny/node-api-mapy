afb5908074c8d01c28e6b1cfaf534189b06d6228
Michal Rozumny <rozumny@hotmail.com>
2017-05-07 11:11:46 +0200

    connect to correct db

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var mongoose = require('mongoose');
-mongoose.connect('localhost:27017'); // connect to our database
+mongoose.connect('localhost:27017/tim'); // connect to our database
 // mongoose.connect('mongodb://kofola:mbKjdQ1feszJ@127.0.0.1:27017/rm2kofola');
 
 router.get('/', (req, res) => {
